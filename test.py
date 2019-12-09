9b5afeb43f2343545d14ad18658267a4819f5ca4
kylluafox <kylluafox@outlook.com>
2019-12-09 09:49:33 -0500

    configurando o projeto para integraçãoc om heroku

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Mensagem para o Cabeçalho da Página. Okay")
+        self.assertRegex(result.data.decode(), "Praticando o curso Devops essentials !")
 
 
 if __name__ == "__main__":
